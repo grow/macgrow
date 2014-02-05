79538c06ad677d41aedd754e4e3b569ee40e5b40
jeremydw <jeremydw@grow.io>
2014-02-05 15:00:21 -0800

    License macgrow under the MIT License.

--- build_release.sh
+++ build_release.sh
@@ -5,7 +5,6 @@ VERSION=`cat pygrow/VERSION`
 cd dist
 chmod +x Grow\ SDK.app/Contents/Resources/cocoasudo
 chmod +x Grow\ SDK.app/Contents/Resources/pygrow/grow/cli.py
-chmod +x Grow\ SDK.app/Contents/Resources/macgrow_cli.py
 zip -r Grow-SDK-${VERSION}.zip Grow\ SDK.app
 cd ..
 echo "Built: dist/Grow-SDK-${VERSION}.zip"
